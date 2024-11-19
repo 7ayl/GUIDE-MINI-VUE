6f1fc98195ae3fb8b7596fb04af0b324b594de7c
7ayl <7ayl@qq.com>
2024-11-19 23:41:10 +0800

    minivue-实现解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -7,41 +7,98 @@ const enum TagType{
 
 export function baseParse(content: string){
   const context = createParserContext(content);
-  return createRoot(parseChildren(context))
+  return createRoot(parseChildren(context, []))
 }
 
-function parseChildren(context){
+function parseChildren(context, ancestors){
   const nodes: any = []
-    
-  let node
+
+  while(!isEnd(context, ancestors)){
+    let node
+    const s = context.source
+    if(s.startsWith("{{")){
+      node = parseInterpolation(context)
+    }else if(s[0] === "<"){
+      if(/[a-z]/i.test(s[1])){
+        node = parseElement(context, ancestors)
+      }
+    }
+    if(!node){
+      node = parseText(context)
+    }    
+    nodes.push(node)
+  }
+  return nodes
+}
+
+function isEnd(context, ancestors){
+  // 2. 当遇到结束标签的时候
   const s = context.source
-  if(s.startsWith("{{")){
-    node = parseInterpolation(context)
-  }else if(s[0] === "<"){
-    if(/[a-z]/i.test(s[1])){
-      node = parseElement(context)
+  if(s.startsWith("</")){
+    for (let i = ancestors.length - 1; i >= 0; i--) {
+      const tag = ancestors[i].tag
+      if(startsWithEndTagOpen(s, tag)){
+        return true
+      }
     }
   }
+//   if(parentTag && s.startsWith(`</${parentTag}>`)){
 
-  nodes.push(node)
-
-  return nodes;
+//     return true
+//   }
+  // 1. source 有值的时候
+  return !s
 }
 
-function parseElement(context: any){
-  // Implent
-  // 1. 解析 tag
-  const element =  parseTag(context, TagType.Start)
-  parseTag(context, TagType.End)
+function parseText(context: any){
+  let endIndex = context.source.length
+  let endTokens = ["<","{{"]
+  for (let i = 0; i < endTokens.length; i++) {
+    const index = context.source.indexOf(endTokens[i])
+    if(index !== -1 && endIndex > index){
+     endIndex = index
+    }
+  }
+  // 1.获取content
+  const content = parseTextData(context, endIndex)
 
-  console.log("------",context.source);
+  console.log("content-------", content);
+
+  return {
+    type:NodeTypes.TEXT,
+    content
+  }
+}
+
+function parseTextData(context: any, length){
+  const content = context.source.slice(0, length)
+  // 2.推进
+  advanceBy(context, length)
+  return content
+}
+
+function parseElement(context: any, ancestors){
+  const element: any =  parseTag(context, TagType.Start)
+  ancestors.push(element)
+  element.children = parseChildren(context, ancestors)
+  ancestors.pop()
+
+  if(startsWithEndTagOpen(context.source, element.tag)){
+    parseTag(context, TagType.End)
+  }else{
+    throw new Error(`缺少结束标签：${element.tag}`);
+  }
 
   return element;
 }
 
+function startsWithEndTagOpen(source, tag){
+  return source.startsWith("</") && source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
+}
+
 function parseTag(context: any, type: TagType){
+  // <div></div>
   const match: any = /^<\/?([a-z]*)/i.exec(context.source)
-  console.log(match);
   const tag = match[1]
   // 2. 删除处理完成的代码
   advanceBy(context, match[0].length)
@@ -66,9 +123,10 @@ function parseInterpolation(context){
 
   const rawContentLength = closeIndex - openDelimiter.length
 
-  const rawContent = context.source.slice(0, rawContentLength)
+  const rawContent = parseTextData(context, rawContentLength)
+
   const content = rawContent.trim()
-  advanceBy(context, rawContentLength + closeDelimiter.length)
+  advanceBy(context, closeDelimiter.length)
 //   context.source = context.source.slice(rawContentLength + closeDelimiter.length)
 
   return {

ea55c7ef9b8f6010999db69fe45bc54f4397e8a8
7ayl <7ayl@qq.com>
2024-11-21 14:09:02 +0800

    minivue-实现代码生成string类型

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,10 +1,13 @@
-import { NodeTypes } from "./ast";
-
-export function transform(root, options){
+export function transform(root, options = {}){
   const context = createTransformContext(root, options)
-  // 遍历 - 深度优先搜索
   traverseNode(root,context)
-  // 修改 text content
+
+  createRootCodegen(root)
+  // root.codegenNode
+}
+
+function createRootCodegen(root: any){
+  root.codegenNode = root.children[0]
 }
 
 function createTransformContext(root: any, options: any) {

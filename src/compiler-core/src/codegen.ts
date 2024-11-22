a32be4632a43e22c9225db1600940711d9f1e219
7ayl <7ayl@qq.com>
2024-11-22 11:04:17 +0800

    minivue-实现代码生成插值类型

--- src/compiler-core/src/codegen.ts
+++ src/compiler-core/src/codegen.ts
@@ -1,13 +1,17 @@
+import { NodeTypes } from "./ast"
+import { helperMapName, TO_DISPLAY_STRING } from "./runtimehelpers"
+
 export function generate(ast){
   const context = createCodegenContext()
   const { push } = context
-  push("return ")
+ 
+  genFunctionPreamble(ast, context)
 
   const functionName = "render"
   const args = ["_ctx", "_cache"]
   const signature = args.join(", ")
 
-  push(` function ${functionName}(${signature}){`)
+  push(`function ${functionName}(${signature}){`)
   push("return ")
 
   genNode(ast.codegenNode, context)
@@ -18,18 +22,61 @@ export function generate(ast){
   }
 }
 
+function genFunctionPreamble(ast: any, context: any) {
+  const { push } = context
+  const VueBinging = "Vue"
+  const aliasHelper = (s) => `${helperMapName[s]}:_${helperMapName[s]}`
+  if(ast.helpers.length > 0){
+    push(`const { ${ast.helpers.map(aliasHelper).join(", ")} } = ${VueBinging}`)
+  }
+  push("\n")
+  push("return ")
+}
+
 function createCodegenContext(): any {
   const context = {
     code: "",
     push(source){
       context.code += source
+    },
+    helper(key){
+      return `_${helperMapName[key]}`
     }
   }
   return context
 }
 
 function genNode(node: any, context){
-    const { push } = context
-    push(`'${node.content}'`)
+  switch (node.type) {
+    case NodeTypes.TEXT:
+      genText(node,context)
+      break;
+    case NodeTypes.INTERPOLATION:
+      genInterpolation(node, context)
+      break;
+    case NodeTypes.SIMPLE_EXPRESSION:
+      genExpression(node, context)
+      break;
+    default:
+      break;
+  }
 }
+
+function genExpression(node: any, context: any) {
+  const { push } = context
+  push(`${node.content}`)
+}
+
+function genInterpolation(node: any, context: any) {
+  const { push, helper } = context
+  push(`${helper(TO_DISPLAY_STRING)}(`)
+  genNode(node.content, context)
+  push(")")
+}
+
+function genText(node: any, context: any) {
+  const { push } = context
+  push(`'${node.content}'`)
+}
+
 
--- src/compiler-core/tests/codegen.spec.ts
+++ src/compiler-core/tests/codegen.spec.ts
@@ -1,6 +1,7 @@
 import { generate } from "../src/codegen"
 import { baseParse } from "../src/parse"
 import { transform } from "../src/transform"
+import { transformExpression } from "../src/transforms/transformExpression"
 
 describe('codegen', () => {
   it('string', () => {
@@ -10,6 +11,15 @@ describe('codegen', () => {
     // 快照(string)
     // 1.抓bug
     // 2.有意的
+    expect(code).toMatchSnapshot()
+  })
+
+  it('interpolation', () => {
+    const ast = baseParse("{{message}}") 
+    transform(ast, {
+      nodeTransforms:[transformExpression]
+    })
+    const { code } = generate(ast) 
     expect(code).toMatchSnapshot()
   })
 })
